7953d6d9e233501bfc63da737363dde8eedc3cab
Ervin117 <ervinggg04@gmail.com>
2025-04-28 12:09:26 -0600

    entrega final

--- Laboratorio6/Laboratorio6/main.cpp
+++ Laboratorio6/Laboratorio6/main.cpp
@@ -16,16 +16,24 @@ void setup();
 void serialUART();
 void serialLECT(char letra);
 void mostrarLect(char dato);
+void stringTermi(char* oracion); 
+void ADC_1(); 
+uint8_t lecADC(); 
+void menu(); 
+void num(uint8_t numero);
 
 /****************************************/
 // Main Function
 int main (void)
 {
 	setup();
-	serialLECT('e');
+	serialLECT('E');
+	serialLECT('G');
+	serialLECT('3');
+	stringTermi("\n El delay es del diablo");
+	menu();
 	while (1)
 	{
-		
 	}
 }
 
@@ -34,11 +42,11 @@ int main (void)
 void setup()
 {
 	cli(); 
-	serialUART();
 	DDRB = 0xFF; 
+	serialUART();
+	ADC_1(); 
 	sei();
 }
-
 void serialUART()
 {
 	DDRD |= (1<< DDD1); 
@@ -61,9 +69,72 @@ void mostrarLect(char dato)
 	PORTB = dato; 
 }
 
+void stringTermi(char* oracion)
+{
+	for (uint8_t i = 0; *(oracion+i) != '\0'; i++)
+	{
+		serialLECT(*(oracion+i));
+	}
+}
+
+void ADC_1()
+{
+	//ADMUX = 0;
+	ADMUX |= (1<<REFS0) | (1<<ADLAR) | (1 << MUX0) |  (1 << MUX1) | (1<<MUX2); //
+	
+	//ADCSRA = 0;
+	ADCSRA |= (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2) | (1<<ADEN);// |  (1<<ADIE) ;
+	ADCSRA |= (1<<ADSC);
+}
+
+uint8_t lecADC()
+{
+	ADCSRA |= (1<<ADSC); //Empezar conversión
+	while (ADCSRA & (1<<ADSC)); 
+	return ADCH; //Leer los valores altos
+}
+
+void menu()
+{
+	stringTermi("\n\nMenu:\n");
+	stringTermi("1. Leer Potenciometro\n");
+	stringTermi("2. Enviar ASCII\n");
+	stringTermi("Seleccione una opcion:\n");
+}
+
+void num(uint8_t numero)
+{
+	//se separa el numero sus cifras correspondientes
+	uint8_t centenas = numero / 100; //saber centenas
+	uint8_t decenas = (numero % 100) / 10; //saber decenas
+	uint8_t unidades = numero % 10;	//saber unidades
+	
+	//Es el punto de origen del conteo
+	serialLECT(centenas + '0');
+	serialLECT(decenas + '0');
+	serialLECT(unidades + '0');
+}
 /****************************************/
 // Interrupt routines
 ISR(USART_RX_vect)
 {
-	mostrarLect(UDR0);
-}
+	//mostrarLect(UDR0);
+	uint8_t opcion = UDR0; // Capturar dato recibido
+
+	if (opcion == '1')
+	{
+		uint8_t lectura = lecADC(); // Leer ADC7
+		num(lectura); // Enviar como carácter ASCII
+		stringTermi("\n"); // Nueva línea para orden
+	}
+	else if (opcion == '2')
+	{
+		stringTermi("\nEnviando ASCII...\n");
+	}
+	else
+	{
+		stringTermi("\nOpcion invalida.\n");
+	}
+	
+	menu(); // Volver a mostrar el menu
+} 
